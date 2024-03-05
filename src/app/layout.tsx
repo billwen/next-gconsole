51cbacd3e21715e302ebcc20b5a23e5535443ed3
Gang Wen <gang.wen@ingka.ikea.com>
2024-03-05 16:03:32 +0100

    fixed stylesheet location

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React, {ReactNode} from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../components/globals.css";
+import "./globals.css";
 import {Toaster} from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
